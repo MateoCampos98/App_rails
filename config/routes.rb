f30710710e9cbedc0f4a0b24ebfce4b95b6e08aa
Mateo Campos <campos129823@gmail.com>
2024-09-14 23:36:51 -0500

    Improving our app, we need to add i18n.es.yml, #1test error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create], path: '/register', path_names: { new: '/' }
+    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+  end
+
   resources :categories, except: :show  
   resources :products, path: "/"
 
-  namespace :authentication, path: '', as: '' do
-    resources :users, only: [:new, :create]
-    resources :sessions, only: [:new, :create]
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
