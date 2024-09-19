3037ef594b698f2e13b2528f1c27772b9de41df2
Mateo Campos <campos129823@gmail.com>
2024-09-18 22:48:16 -0500

    Messages using rails flash, in the partial, and CSS styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
+
   include Pagy::Frontend
+
+  def flash_class(key)
+    case key
+    when 'notice'
+      'flash-notice'  # Clase para notificaciones
+    when 'alert'
+      'flash-alert'    # Clase para alertas
+    else
+      'flash-info'     # Clase por defecto
+    end
+  end
 end
